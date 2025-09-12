36d4c8b7d360869e43b0a9fc19f942e05fa16d0d
Edgard Alvarado <Edgard-0211@hotmail.com>
2025-09-11 23:09:40 -0500

    saving main.java

--- Main.java
+++ Main.java
@@ -13,14 +13,16 @@ public class Main {
 
         System.out.println(PLATFORM_NAME + " v" + VERSION);
 
-        Movie movie = new Movie();
-        movie.title = CustomUtils.GetString("Ingrese titulo de la película:");
-        movie.description = CustomUtils.GetString("Ingrese descripción de la película:");
-        // movie.premierYear=2025;
-        movie.premiereDate = LocalDate.of(2025, 7, 23);
-        movie.genre = CustomUtils.GetString("Ingrese Genero de la película:");
-        movie.duration = CustomUtils.GetInt("Ingrese duración de la pelúcula:");
+        String title = CustomUtils.GetString("Ingrese titulo de la película:");
+        String description = CustomUtils.GetString("Ingrese descripción de la película:");
+        int premierYear= CustomUtils.GetInt("Ingrese el año de estreno de la película:");
+        //movie.premiereDate = LocalDate.of(2025, 7, 23);
+        String genre = CustomUtils.GetString("Ingrese Genero de la película:");
+        int duration = CustomUtils.GetInt("Ingrese duración de la película:");
+        double rate =CustomUtils.GetDecimal("ingrese el rating de la película:");
         
+        
+        Movie movie = new Movie(title, duration, genre);
         movie.Rate(4.2);
 
 
